73255bde83fb93d18b004cc638a74b23015b1422
Danilovich Iliya <danilovich-iliya@yandex.ru>
2023-01-22 00:58:47 +0700

    build: Собрать проект webpack ver.1

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,4 @@
+import './index.css';
 import { FormValidator } from '../components/FormValidator.js';
 import {
     initialCards,
@@ -8,7 +9,9 @@ import {
     profileNameSelector,
     profileProfessionSelector,
     buttonEditProfile,
-    profile
+    profile,
+    inputProfileName,
+    inputProfileProfession
 } from '../utils/constants.js';
 import Card from '../components/Card.js';
 import Section from '../components/Section.js';
